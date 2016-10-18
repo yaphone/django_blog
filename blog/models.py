e4388433ce19db743986cc1b08cc43ddd68c07a8
yaphone <zhouyaphone@163.com>
2016-10-18 23:51:52 +0800

    some add

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    blog_title = models.CharField(max_length = 128)
+    blog_content = models.TextField(max_length=1000000)
+    update_time = models.DateTimeField()
+    modify_time = models.DateTimeField()
+    blog_type = models.CharField(max_length = 128)
+    blog_tag = models.CharField(max_length = 128)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import Blog
+
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello World!")
+
+def blog(request):
+    blog_list = Blog.objects.order_by('-update_time')[:5]
+    context = {'blog_list': blog_list}
+    return render(request, 'blog/index.html', context)

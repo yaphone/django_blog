508350237e2b6a25d509b7db74a5e6fa26bb56e6
yaphone <zhouyaphone@163.com>
2017-01-21 17:34:18 +0800

    微信开发

---
+++ wechat/util/tests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from django.test import TestCase
+
+import time
+
+def test():
+    file = open("test.txt", 'a+')
+    file.write("123")
+    file.close()
+
+if __name__ == '__main__':
+    test()
+
+

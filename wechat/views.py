9e40c9f456e5255864bbc98cdebed3dd5613282b
yaphone <zhouyafeng2013@gmail.com>
2017-01-28 22:56:57 +0800

    涛哥新增功能

--- wechat/views.py
+++ wechat/views.py
@@ -9,12 +9,14 @@ from django.utils import timezone
 from .models import Location
 import time
 import os
-import urllib2,json
+import urllib, urllib2,json
 import json
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
-
 def index(request):
     #return HttpResponse("Hello, world. You're at the polls index.")
     return render(request, 'wechat/index.html')
@@ -45,23 +47,28 @@ def wechatjs(request): #微信JS验证文件
     return render(request, 'wechat/MP_verify_w1ExVKCAt7pVaCbt.txt')
 
 def my_sign(request): #生成签名随机串
-    file = open("/home/github/django_blog/wechat/util/ticket.txt")
-    jsapi_ticket = file.read()[:-1]  #.read()方法会在字符串后加一个换行符，这里去掉
+    #file = open("/home/github/django_blog/wechat/util/ticket.txt")
+    #jsapi_ticket = file.read()[:-1]  #.read()方法会在字符串后加一个换行符，这里去掉
     #jsapi_ticket = 'kgt8ON7yVITDhtdwci0qeTWQwsqkmCCxl3Cird9AlCT5uvbeLnCEawfiV9IBANNlE6pgrZEIqEVWR3O69OEGhQ'
-    url = "http://zhouyafeng.cn/wechat/"  
-    S = sign.Sign(jsapi_ticket, url)
-    ret = S.sign()
+    #url = "http://zhouyafeng.cn/wechat/"
+    #S = sign.Sign(jsapi_ticket, url)
+    #ret = S.sign()
+    ret = {}
     return JsonResponse(ret)
 
 def save_location(request):
     if request.method == "GET":
         latitude = request.GET.get('latitude') #纬度
         longitude = request.GET.get('longitude') #经度
+        ip = request.GET.get('ip') #用户IP地址
+        print latitude
         time = timezone.now()
-        location = Location(latitude=latitude, longitude=longitude, time=time)
+        location = Location(latitude=latitude, longitude=longitude, time=time, ip=ip)
         location.save()
         return JsonResponse({"status": "OK"})
 
+
+@login_required
 def show_location(request):
     if request.method == "GET":
         locations = Location.objects.order_by('-time')
@@ -80,4 +87,13 @@ def get_specific_location(request):
     latitude = location.latitude
     longitude = location.longitude
     return JsonResponse({"longitude": longitude, "latitude": latitude})
+
+def news(request):
+    return render(request, "wechat/news.html")
+
+def get_news(request):
+    json_res = urllib.urlopen(
+        "http://op.juhe.cn/onebox/news/query?key=2a0030ef30ad3c7c65173371aa490a1e&q=%E6%99%AE%E4%BA%AC")
+    res = json.load(json_res)
+    return JsonResponse(res)
 
